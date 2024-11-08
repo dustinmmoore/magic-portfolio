e9720a432c8fc425351d3864f79462adb6b11ef6
dustinmmoore <dustinmmoore@icloud.com>
2024-11-07 20:01:26 -0500

    blog title change

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -176,7 +176,7 @@ I’m a front-end developer with a focus on creating responsive, user-driven web
 
 const blog = {
     label: 'Blog',
-    title: 'Writing about design and tech...',
+    title: 'Writing about stuff...',
     description: `Read what ${person.name} has been up to recently`
     // Create new blog posts by adding a new .mdx file to app/blog/posts
     // All posts will be listed on the /blog route
