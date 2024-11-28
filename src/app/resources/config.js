86966525e93890886e647dba58c0db15b535ad3e
dustinmmoore <dustinmmoore@icloud.com>
2024-11-28 15:44:40 -0500

    bg color

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -30,15 +30,16 @@ const effects = {
 }
 
 const style = {
-    theme:       'dark',         // dark | light
-    neutral:     'gray',         // sand | gray | slate
-    brand:       'aqua',         // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
-    accent:      'yellow',       // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
-    solid:       'contrast',     // color | contrast
-    solidStyle:  'flat',         // flat | plastic
-    border:      'playful',      // rounded | playful | conservative
-    surface:     'translucent',  // filled | translucent
-    transition:  'all'           // all | micro | macro
+    theme:       'dark',        // dark | light
+    neutral:     'gray',        // sand | gray | slate
+    brand:       'cyan',        // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+    accent:      'indigo',      // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+    solid:       'contrast',    // color | contrast
+    solidStyle:  'flat',        // flat | plastic
+    border:      'playful',     // rounded | playful | conservative
+    surface:     'translucent', // filled | translucent
+    transition:  'all',         // all | micro | macro
+    scaling:     '100',         // 90 | 95 | 100 | 105 | 110
 }
 
 const display = {
