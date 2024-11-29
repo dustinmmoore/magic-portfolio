77ac4a4ef70046bff0200a0a2b6cbb43486d29b4
dustinmmoore <dustinmmoore@icloud.com>
2024-11-28 21:57:36 -0500

    color change

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -13,7 +13,7 @@ const routes = {
     '/':        true,
     '/about':   true,
     '/work':    true,
-    '/blog':    true,
+    '/blog':    false,
     '/gallery': false,
 }
 
@@ -25,17 +25,17 @@ const protectedRoutes = {
 
 const effects = {
     gradient: true,
-    dots:     true,
-    lines:    false,
+    dots:     false,
+    lines:    true,
 }
 
 const style = {
     theme:       'dark',        // dark | light
-    neutral:     'gray',        // sand | gray | slate
-    brand:       'cyan',        // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
-    accent:      'indigo',      // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+    neutral:     'slate',        // sand | gray | slate
+    brand:       'aqua',        // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+    accent:      'aqua',        // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
     solid:       'contrast',    // color | contrast
-    solidStyle:  'flat',        // flat | plastic
+    solidStyle:  'plastic',     // flat | plastic
     border:      'playful',     // rounded | playful | conservative
     surface:     'translucent', // filled | translucent
     transition:  'all',         // all | micro | macro
